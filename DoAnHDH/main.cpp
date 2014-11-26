92abcd2675136eb0ce7324106ec360fe0e1c864c
hodachop93 <hodachop93@gmail.com>
2014-11-26 22:31:43 +0700

    project do an
    update 26/11

---
+++ DoAnHDH/main.cpp
@@ -0,0 +1,51 @@
+﻿// DoAnHDH.cpp : Defines the entry point for the console application.
+
+
+#include "stdafx.h"
+#include <stdlib.h>
+#include <gl/glut.h>
+#include "MyShape.h"
+#include <Windows.h>
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+void display(void)
+{
+	glColor3f(1.0, 1.0, 1.0);
+	MyGraphics::MyShape::MyRotate(15, 0, 0, 1);
+
+	MyGraphics::MyShape::MyRectangle_Filled(100, 100, 200, 200);
+	MyGraphics::MyShape::MyRectangle(100, 100, 400, 400);
+	MyGraphics::MyShape::MyCircle(200, 200, 100);
+
+	
+	
+	
+}
+/* hàm thực hiện các khởi tạo */
+void init(void)
+{
+	/* chọn màu để xóa nền (tức là sẽ phủ nền bằng màu này)  */
+	glClearColor(0.0, 0.0, 0.0, 0.0); /* màu đen */
+	/* thiết lập các thông số cho view */
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	return;
+}
+int main(int argc, char** argv)
+{
+	
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); /* khởi tạo chế độ vẽ
+												 single buffer và hệ màu RGB */
+	glutInitWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT); /* khởi tạo window */
+	glutInitWindowPosition(100, 100);  /* khởi tạo window tại ví trí
+									   (100,100) trên screen */
+	glutCreateWindow("DoAn"); /* tên của window là ‘rectangle’ */
+	init();  /* khởi tạo một số chế độ đồ họa */
+	glutDisplayFunc(display); /* thiết lập hàm vẽ là hàm display() */
+	glutMainLoop(); /* bắt đầu chu trình lặp thể hiện vẽ */
+	return 0;
+}
---
+++ Include/MyShape.h
@@ -0,0 +1,32 @@
+﻿typedef float Affine[3][3];
+typedef float Point[2];
+namespace MyGraphics
+{
+	class MyShape
+	{
+	public:
+		//Vẽ hình chữ nhật
+		static __declspec(dllexport) void MyRectangle(float x0, float y0, float x1, float y1);
+		//Vẽ hình tròn
+		static __declspec(dllexport) void MyCircle(float xc, float yc, float r);
+		//Dịch chuyển đối tượng
+		static __declspec(dllexport) void MyTranslate(float x, float y, float z);
+		//Quay đối tượng
+		static __declspec(dllexport) void MyRotate(float gocQuay, float x, float y, float z);
+		//Phép biến đổi tỉ lệ
+		static __declspec(dllexport) void MyScale(float x, float y, float z);
+		static __declspec(dllexport) void MyLine(float x0, float y0, float x1, float y1);
+		static __declspec(dllexport) void Translate_x(float &x0, float dx);
+		static __declspec(dllexport) void Translate_y(float &y0, float dy);
+		static __declspec(dllexport) void MyRectangle_Filled(float x0, float y0, float x1, float y1);
+		static __declspec(dllexport) void PointToAffine(Point A, Affine &B);
+		static __declspec(dllexport) void Multiply(Affine A, Affine B, Affine &C, int m, int n);
+		static __declspec(dllexport) void RotatePoint_O(Affine &T, float fi);
+		static __declspec(dllexport) void TinhTien(Affine &T, float x, float y);
+		static __declspec(dllexport) void Nghieng(Affine &T, float h, float g);
+		static __declspec(dllexport) void CoDan(Affine &T, float Sx, float Sy);
+	private:
+		static int Round(double a);
+		
+	};
+}
